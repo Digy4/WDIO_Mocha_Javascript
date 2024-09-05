18c8290647fdfe8e78386f39cbbd1f3c8ebf11d9
d-shafi-khan <87249412+d-shafi-khan@users.noreply.github.com>
2024-09-05 11:52:51 +0100

    Adding a failing test

--- src/test/specs/example.e2e.js
+++ src/test/specs/example.e2e.js
@@ -11,6 +11,13 @@ describe('Login:', () => {
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
     });
+
+    it('should fail assertion when login with valid credentials', async () => {
+        await LoginPage.open();
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+
+        await expect(SecurePage.flashAlert).toBeExisting();
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You are not logged into a secure area!');
+    });
 });
-
-
